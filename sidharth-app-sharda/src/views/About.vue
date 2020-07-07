dabdba8c448b67302fcb95efe679c8d04b4a9a15
Sidharth13 <sidbhasin13@gmail.com>
2020-07-07 21:17:19 +0530

    Sharda-Website-vue

---
+++ sidharth-app-sharda/src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <br />
+    <br />
+    <h3>
+      <br />The School of Engineering and technology (SET) is an open platform
+      for <br />diverse voices where teaching runs parallel to the real world
+      <br />distinguished and students are groomed to join the global workforce.
+      SET is <br />distinguished as one of the top-ranked engineering schools in
+      <br />India. The students at SET benefit through the professional grooming
+      of <br />renowned faculty and industry experts having experience of
+      <br />tackling pressing engineering problems. Students discover their
+      passion in <br />one of the various offered Engineering majors at the
+      <br />School of Engineering and technology. A student-centric pedagogy,
+      <br />project-based approach and design-driven curriculum provides
+      <br />students with an inclination for complex problem solving, design,
+      <br />innovation, and a passion for learning. The mission of the School of
+      <br />Engineering and technology through its various programmes is to
+      educate <br />wellintegrated individuals who possess technical and social
+      competence to <br />succeed in professional arenas and design solutions
+      <br />for global problems.
+    </h3>
+    <div>
+      <b-button>Our Faculties</b-button> <br /><br />
+      <b-button>Our Students</b-button>
+    </div>
+  </div>
+</template>
---
+++ sidharth-app-sharda/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <br />
+    <br />
+    <h2>SCHOOL OF ENGINEERING AND TECHNOLOGY</h2>
+    <br />
+    <br />
+    <h3>PRESENTS</h3>
+    <br />
+    <br />
+    <img alt="Vue logo" src="../assets/books.png" />
+    <br />
+    <br />
+    <h2>LEARNSHALA</h2>
+    <br />
+    <br />
+    <h3>Where learning never stops !!</h3>
+    <br />
+    <br />
+    <div>
+      <b-button>Enter LEARNSHALA</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
